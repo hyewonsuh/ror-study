882a84fe9a3970869fc5f646602804b2cc69ab4e
hyewon <isolato75@gmail.com>
2015-09-06 00:25:15 +0900

    chapter 14.3

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -7,8 +7,11 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class StoreController < ApplicationController
+  skip_before_action :authorize
+
   include CurrentCart
   before_action :set_cart
+
   def index
     @products = Product.order(:title)
   end
