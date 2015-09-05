4009179ebb12ddd5d35152af75e9fc327c274b99
hyewon <isolato75@gmail.com>
2015-09-06 01:00:12 +0900

    chapter 14.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  after_destroy :ensure_an_admin_remains
 
   # bcrypt-ruby gem : password와 password 확인 필드가 같은지도 검증
   has_secure_password
 
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end
 end
