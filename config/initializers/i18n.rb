233c570ef52297c7c0a62c165868bb485e810cd8
hyewon <isolato75@gmail.com>
2015-09-06 02:16:28 +0900

    chapter 15

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,9 @@
+#encoding: utf-8
+
+I18n.default_locale = :en
+
+LANGUAGES = [
+    ['English',     'en'],
+    ['한국어',     'ko']
+
+]
--- config/routes.rb
+++ config/routes.rb
@@ -15,22 +15,21 @@ Depot::Application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :line_items
-  resources :carts
-
-  get "store/index"
-  resources :products
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store'
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
   end
   # ...
 
