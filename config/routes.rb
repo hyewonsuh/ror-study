8dd195a955b8dc49365c61b6ff29967f8ff11d65
hyewon <isolato75@gmail.com>
2015-09-06 00:12:53 +0900

    chapter 14.2

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,19 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
   resources :orders
   resources :line_items
   resources :line_items
   resources :carts
+
   get "store/index"
   resources :products
 
